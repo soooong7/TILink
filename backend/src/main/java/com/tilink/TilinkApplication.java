a9b897a39742c08daa187360ac191d201c784d6a
soooong7 <dlthdal07@gmail.com>
2026-09-08 18:32:48 +0900

    🎉 init: SpringBoot 프로젝트 초기화

---
+++ backend/src/main/java/com/tilink/TilinkApplication.java
@@ -0,0 +1,13 @@
+package com.tilink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TilinkApplication.class, args);
+	}
+
+}
